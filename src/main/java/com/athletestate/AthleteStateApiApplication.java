0f5b3b653cad569251ce3367907a5970a4cfe56a
Navodya Dissanayake <nawodhyasrilanka2015@gmail.com>
2026-07-22 08:57:11 +0530

    chore: initialize Spring Boot project via Initializer

---
+++ src/main/java/com/athletestate/AthleteStateApiApplication.java
@@ -0,0 +1,13 @@
+package com.athletestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteStateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteStateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athletestate/AthleteStateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athletestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleteStateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
