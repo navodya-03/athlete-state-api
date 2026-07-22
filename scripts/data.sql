4693e6c3abd6781f253f69c4b1b118a1981e5279
Navodya Dissanayake <nawodhyasrilanka2015@gmail.com>
2026-07-22 18:41:49 +0530

    message: chore: add seed data script for local testing

---
+++ scripts/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO athletes(first_name,last_name)
+VALUES
+    ('Navodya','Gimhan'),
+    ('Christopher','Nolan');
+
+
+INSERT INTO workouts(athlete_id,date,focus_area)
+VALUES
+    (1,'2026-7-22','chest'),
+    (2,'2026-7-22','tricep'),
+    (1,'2026-7-22','back'),
+    (2,'2026-7-22','bicep');
