7bc3a00ae0319864e571771528da1429559470b4
Navodya <nawodhyasrilanka2015@gmail.com>
2026-08-04 21:21:17 +0530

    feat: expose GET and POST REST endpoints for athletes

---
+++ src/main/java/com/athletestate/controller/AthleteController.java
@@ -0,0 +1,57 @@
+package com.athletestate.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.athletestate.model.Athlete;
+import com.athletestate.service.AthleteService;
+
+
+@RestController
+@RequestMapping("/api/athletes")
+public class AthleteController {
+
+    private final AthleteService athleteService;
+
+    public AthleteController(AthleteService athleteService){
+        this.athleteService = athleteService;
+    }
+
+
+    @GetMapping
+    public List<Athlete> getAllAthletes(){
+        return athleteService.getAllAthletes();
+    }
+
+    @PostMapping
+
+    public Athlete saveAthlete(@RequestBody Athlete athlete){
+
+        athleteService.createAthlete(athlete);
+
+        return athlete;
+        
+        
+    }
+
+    @PutMapping("/{id}")
+    public Athlete updateAthlete(@PathVariable Integer id, @RequestBody Athlete details){
+        return athleteService.updateAthleteById(id, details);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAthlete(@PathVariable Integer id){
+        athleteService.deleteAthlete(id);
+    }
+
+
+    
+}
